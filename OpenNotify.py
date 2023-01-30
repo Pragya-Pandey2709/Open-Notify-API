bb9208e6c9c154cca94c0837a52890ddcf45ff9e
Pragya Pandey <86335220+Pragya-Pandey2709@users.noreply.github.com>
2023-01-30 12:12:55 +0530

    Create OpenNotify.py

---
+++ OpenNotify.py
@@ -0,0 +1,44 @@
+pip install requests
+import requests
+response = requests.get("https://api.open-notify.org/astros.json")
+print(response.status_code)
+print(response.json())
+import json
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+jprint(response.json())
+
+parameters = {
+    "lat": 40.71,
+    "lon": -74
+}
+
+response = requests.get("https://api.open-notify.org/iss-pass.json", params=parameters)
+
+jprint(response.json())
+
+pass_times = response.json()['response']
+jprint(pass_times)
+
+risetimes = []
+
+for d in pass_times:
+    time = d['risetime']
+    risetimes.append(time)
+
+print(risetimes)
+
+from datetime import datetime
+
+times = []
+
+for rt in risetimes:
+    time = datetime.fromtimestamp(rt)
+    times.append(time)
+    print(time)
+    
+    
